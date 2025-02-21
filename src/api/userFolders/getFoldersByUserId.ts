337a6465a580600f8fe9e00ab82ca5b42a078a08
titenq <titenq@gmail.com>
2025-02-21 12:13:16 -0300

    style media screen

---
+++ src/api/userFolders/getFoldersByUserId.ts
@@ -0,0 +1,35 @@
+import { backendBaseUrl } from '@/helpers/baseUrl';
+import { IGenericError } from '@/interfaces/errorInterface';
+import { IUserFolder } from '@/interfaces/userFoldersInterface';
+
+const getFoldersByUserId = async (userId: string): Promise<IUserFolder | IGenericError> => {
+  try {
+    const response = await fetch(`${backendBaseUrl}/folders/${userId}`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    });
+
+    if (!response.ok) {
+      const data: IGenericError = await response.json();
+
+      return data;
+    }
+
+    const data: IUserFolder = await response.json();
+
+    return data;
+  } catch (_error) {
+    const errorMessage: IGenericError = {
+      error: true,
+      message: 'erro ao buscar as pastas do usuário',
+      statusCode: 400
+    }
+
+    return errorMessage;
+  }
+};
+
+export default getFoldersByUserId;
